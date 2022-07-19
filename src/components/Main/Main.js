301bb93bf3afe23d42f6dd9faa92eb79bdd038db
Vika Dovgal <vikadovgal@yandex.ru>
2022-07-19 11:39:52 +0400

    bugfix: правки фона Techs на разрешении 320px

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import AboutProject from '../AboutProject/AboutProject.js';
 import Techs from '../Techs/Techs.js';
 import AboutMe from '../AboutMe/AboutMe.js';
 import Portfolio from '../Portfolio/Portfolio.js';
+import useWindowWidth from '../../hooks/getWindowWidth.js';
 
 function Main() {
   return (
@@ -16,7 +17,13 @@ function Main() {
       <div className="section__main section__main_theme_white">
         <AboutProject />
       </div>
-      <div className="section__main section__main_theme_grey">
+      <div
+        className={`section__main ${
+          useWindowWidth() > 320
+            ? 'section__main_theme_grey'
+            : 'section__main_theme_white'
+        }`}
+      >
         <Techs />
       </div>
       <div className="section__main section__main_theme_white">
